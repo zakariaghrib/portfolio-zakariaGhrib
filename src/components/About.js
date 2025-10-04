e8fdff8d0f33ccd7257b9c0be640eff5b5e377a4
zakariaghrib <zikoghb12@gmail.com>
2025-10-05 00:13:20 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,13 +4,13 @@ const About = () => {
   return (
     <section id="à-propos" className="py-20 bg-white">
       <div className="container mx-auto px-6 text-center">
-        <h2 className="text-3xl font-bold mb-6">À Propos de Moi [cite: 3]</h2>
+        <h2 className="text-3xl font-bold mb-6">À Propos de Moi </h2>
         <div className="w-24 h-1 bg-indigo-600 mx-auto mb-8"></div>
         <p className="max-w-3xl mx-auto text-gray-700 text-lg leading-relaxed">
-          Futur ingénieur en génie informatique, j'ai de solides compétences en gestion de projet, en bases de données et en plusieurs langages de programmation, avec des certifications en Node.js, Express.js, MongoDB et JavaScript. [cite: 4]
+          Futur ingénieur en génie informatique, j'ai de solides compétences en gestion de projet, en bases de données et en plusieurs langages de programmation, avec des certifications en Node.js, Express.js, MongoDB et JavaScript. 
         </p>
         <p className="max-w-3xl mx-auto text-gray-700 text-lg leading-relaxed mt-4">
-          Motivé et proactif, je suis prêt à m'investir dans des projets innovants. [cite: 5]
+          Motivé et proactif, je suis prêt à m'investir dans des projets innovants. 
         </p>
       </div>
     </section>
