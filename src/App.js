5156f0a0b6649ccde9ef06d3879f310c032ceda9
zakariaghrib <zikoghb12@gmail.com>
2025-10-04 19:54:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import About from './components/About'; // Importer le composant About
+import Skills from './components/Skills'; // Importer le composant Skills
 import './App.css';
 
 function App() {
   return (
-    <div className="font-sans text-gray-900">
+    <div className="font-sans text-gray-900 bg-gray-50">
       <Header />
       <main>
         <Hero />
+        <About />   {/* Ajouter le composant About ici */}
+        <Skills />  {/* Ajouter le composant Skills ici */}
         {/* Les autres sections viendront ici */}
-        {/* <About /> */}
-        {/* <Skills /> */}
-        {/* etc. */}
       </main>
     </div>
   );
