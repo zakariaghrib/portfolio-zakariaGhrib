46598c3ed2bb1841e858bdf452ce6d5b30d4a903
zakariaghrib <zikoghb12@gmail.com>
2025-10-05 21:47:42 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
         <h2 className="text-3xl font-bold mb-6 text-gray-900 dark:text-white">À Propos de Moi</h2>
         <div className="w-24 h-1 bg-indigo-600 mx-auto mb-8"></div>
         <p className="max-w-3xl mx-auto text-gray-700 dark:text-gray-300 text-lg leading-relaxed">
-          [cite_start]Futur ingénieur en génie informatique, j'ai de solides compétences en gestion de projet, en bases de données et en plusieurs langages de programmation, avec des certifications en Node.js, Express.js, MongoDB et JavaScript. [cite: 12] [cite_start]Motivé et proactif, je suis prêt à m'investir dans des projets innovants. [cite: 13]
+          En tant que futur ingénieur en génie informatique , je possède de solides compétences en développement full-stack, avec une expertise particulière dans la stack MERN (MongoDB, Express.js, React.js, Node.js). Mon expérience comprend la gestion du cycle de vie complet du développement d'applications , comme l'illustre la création d'un système de gestion scolaire de A à Z. Motivé, proactif et doté d'une forte capacité à résoudre les problèmes , je suis prêt à m'investir dans des projets innovants et à collaborer efficacement en équipe pour optimiser la performance des applications.
         </p>
       </div>
     </section>
